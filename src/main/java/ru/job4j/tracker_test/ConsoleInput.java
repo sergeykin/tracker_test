f21c59854aa13ee219c8a5885fe05c098a530a2a
sergeykin <sergeykinn@mail.ru>
2021-05-30 10:43:43 +0300

    2. Сканиварование проекта [#1070]

--- src/main/java/ru/job4j/tracker_test/ConsoleInput.java
+++ src/main/java/ru/job4j/tracker_test/ConsoleInput.java
@@ -1,8 +1,11 @@
 package ru.job4j.tracker_test;
 
 
+import org.springframework.stereotype.Component;
+
 import java.util.Scanner;
 
+@Component
 public class ConsoleInput  {
 
     private Scanner scanner = new Scanner(System.in);
--- src/main/java/ru/job4j/tracker_test/StartUI.java
+++ src/main/java/ru/job4j/tracker_test/StartUI.java
@@ -1,6 +1,9 @@
 package ru.job4j.tracker_test;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class StartUI {
 
     private Store store;
