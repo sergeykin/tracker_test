ea22213e48c2fea9c5d80a62271793a85039f26c
sergeykin <sergeykinn@mail.ru>
2021-05-30 11:01:52 +0300

    4. Время жизни (Scope) [#261277]

--- src/main/java/ru/job4j/tracker_test/SpringDI.java
+++ src/main/java/ru/job4j/tracker_test/SpringDI.java
@@ -7,9 +7,18 @@ public class SpringDI {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
         context.scan("ru.job4j.tracker_test");
         context.refresh();
+
+        Store store = context.getBean(Store.class);
+        store.add("Petr Arsentev");
+        Store another = context.getBean(Store.class);
+        another.add("another");
+        another.getAll().forEach(System.out::println);
+
         StartUI ui = context.getBean(StartUI.class);
         ui.add();
-        ui.add();
-        ui.print();
+//        ui.add();
+//        ui.print();
+        StartUI uiAnother = context.getBean(StartUI.class);
+        uiAnother.print();
     }
 }
